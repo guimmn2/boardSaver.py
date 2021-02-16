7589a42c0e4236ba8ff55b2bd80af105281a33b2
guimmn2 <gmnss@protonmail.com>
2021-02-16 21:25:57 +0000

    login, scroll and getimgurl functions made

--- boardSaver.py
+++ boardSaver.py
@@ -6,48 +6,55 @@ from bs4 import BeautifulSoup
 
 browser = webdriver.Firefox()
 
-init_url = ("https://www.pinterest.com/login")
-url = input("Please input a pinterest board url")
+#init_url = ("https://www.pinterest.com/login")
+#url = input("Please input a pinterest board url")
+
+init_url = "https://www.pinterest.com/login/"
+url = "https://www.pinterest.com/ninosc298/art-deco"
 
 
-print("login to your account")
-email = input("email:")
-password = input("password:")
+#print("login to your account")
+#email = input("email:")
+#password = input("password:")
 
+def login():
+    browser.get(init_url)
+    time.sleep(1)
+    log_email = browser.find_element_by_id("email")
+    log_pw = browser.find_element_by_id("password")
+    log_email.clear()
+    log_pw.clear()
+    log_email.send_keys("ninosc298@hotmail.com")
+    log_pw.send_keys("Guilhas123#")
+    log_pw.send_keys(u'\ue007')
 
-browser.get(init_url)
-time.sleep(1)
-log_email = browser.find_element_by_id("email")
-log_pw = browser.find_element_by_id("password")
-log_email.clear()
-log_pw.clear()
-log_email.send_keys("ninosc298@hotmail.com")
-log_pw.send_keys("Guilhas123#")
-log_pw.send_keys(u'\ue007')
-
+login()
 browser.get(url)
 
-lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-match=False
-while(match==False):
-        lastCount = lenOfPage
-        time.sleep(1)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
-            match=True
-
-source_data = browser.page_source
-
-soup = BeautifulSoup(source_data, 'lxml')
-
-imgs = soup.find_all('img')
-print(imgs)
 
 
 
+def scrollDown():
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    match=False
+    while(match==False):
+            lastCount = lenOfPage
+            time.sleep(1)
+            lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            if lastCount==lenOfPage:
+                match=True
 
+def getImgUrls():
+    source_data = browser.page_source
+    soup = BeautifulSoup(source_data, 'lxml')
+    imgs = soup.find_all('img')
+    for img in imgs:
+        problem = img['src'].split('/')[3]
+        url_clean = img['src'].replace(problem, 'originals')
+        print(url_clean)
 
-
+scrollDown()
+getImgUrls()
 
 
 
